f5701c2595863540a9c872426a39f50f83381ec0
Felix Biego <45796740+fbiego@users.noreply.github.com>
2019-07-02 14:13:30 +0300

    Add files via upload

---
+++ stringSplit/stringSplit.ino
@@ -0,0 +1,58 @@
+
+int z = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    String rd = Serial.readStringUntil('\n');
+    stringSplit(rd,',');
+    
+  }
+
+}
+
+void stringSplit(String s, char a){
+  int l = s.length();
+  int c = 0;
+  for (int j = 0; j < l; j++){
+    if (s.charAt(j) == a){
+      c++;
+    }
+  }
+  String split[z];
+  if (c != 0){
+    for (int k = 0; k < c ; k++){
+      if (k >= z){
+        break;
+      }
+      int x = s.indexOf(a);
+      l = s.length();
+      split[k] = s.substring(0,x);
+      s = s.substring(x+1,l);
+      if (k == c-1){
+        split[k+1] = s;
+      }
+    }
+
+    for (int s = 0; s < c+1; s++){
+      if (s >= z){
+        break;
+      }
+      String p = split[s];
+      Serial.print("|");
+      Serial.print(p);
+      Serial.print("|");
+    }
+    Serial.println(";");
+  } else {
+    Serial.println(s);    //delimiter not found
+  }
+  
+
+}
